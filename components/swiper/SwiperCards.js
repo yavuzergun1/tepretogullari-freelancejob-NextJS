1cb9ba7cc7d976b41c63e40bcfe2967748a35eb7
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-01-14 21:31:54 +0300

    swiper settings done

--- components/swiper/SwiperCards.js
+++ components/swiper/SwiperCards.js
@@ -5,8 +5,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-cards";
+import "swiper/css/pagination";
 
-import { EffectCards } from "swiper";
+import { EffectCards, Autoplay, A11y, Pagination } from "swiper";
 import Image from "next/image";
 
 export default function App() {
@@ -15,9 +16,18 @@ export default function App() {
       <Swiper
         effect={"cards"}
         grabCursor={true}
-        modules={[EffectCards]}
+        modules={[EffectCards, Autoplay, Pagination]}
         className="mySwiper"
         loop={true}
+        autoplay={{
+          delay: 3000,
+          pauseOnMouseEnter: true,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
       >
         <SwiperSlide>
           <Image width={800} height={500} src="/images/slider 1.jpg" />
