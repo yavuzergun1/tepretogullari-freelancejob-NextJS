ed53290c7e90d3ed0794e5b47e0092b1a68a0c7b
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-01-16 00:41:56 +0300

    page max-width setted

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <div style={{maxWidth:"1500px", width:"100%"}}>
+          <Main />
+          <NextScript />
+        </div>
       </body>
     </Html>
-  )
+  );
 }
