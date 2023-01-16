225cbacccc3d731e832e4ec9043cf808248b37d7
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-01-16 14:50:50 +0300

    logo container

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header-container w-full h-[150px] border-black-500 border-b-2 flex px-9 py-2">
-      <div className="logo-container w-[395px] flex items-center">
+      <div className="logo-container w-[495px] flex items-center">
         <Image alt="logo" src="/images/ana-logo.png" width="150" height="150" />
         <p className="text-[26px] font-lato font-extrabold">Kemal Tepretoğulları</p>
       </div>
