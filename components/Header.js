cd4238fdb93c861d9b39fc992e3de226978121a2
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-01-16 17:40:25 +0300

    nav links added

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
-    <div className="header-container w-full h-[150px] border-black-500 border-b-2 flex px-9 py-2">
+    <div className="header-container w-full h-[150px] border-black-500 border-b-2 flex justify-between px-9 py-2">
       <div className="logo-container w-[425px] flex items-center text-[rgb(4,16,68)]">
         <Image alt="logo" src="/images/ana-logo.png" width="150" height="150" />
         <p className="text-[26px] font-[NouvelR] font-extrabold">
           Kemal Tepretoğulları
         </p>
+      </div>
+      <div className="w-[550px] flex justify-between items-center font-[NouvelR]">
+        <Link href={"/hakkimizda"}>Hakkımızda </Link>
+        <Link href={"/tarihce"}> Tarihçemiz</Link>
+        <Link href={"/vizyon"}>Vizyon ve Misyon </Link>
+        <Link href={"/markalarimiz"}> Markalarımız</Link>
       </div>
     </div>
   );
