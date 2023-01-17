cddd283b4df4f008fdfd55836fb76f4237ff2d71
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-01-17 17:32:38 +0300

    vizyon done

--- pages/vizyon.js
+++ pages/vizyon.js
@@ -3,11 +3,11 @@ import { getVizyonData } from "../components/FirebaseUtils";
 
 const Vizyon = ({ data }) => {
   return (
-    <div className="mt-24 h-[100vh] font-[NouvelR]">
-      <h1>{data.mainTitle} </h1>
-      <h2>{data.title} </h2>
-      <p>{data.p1} </p>
-      <h2>{data.title2} </h2>
+    <div className="mt-24 h-[100vh] font-[NouvelR] px-16">
+      <h1 className="text-center mb-24">{data.mainTitle} </h1>
+      <h2 className="mb-7">{data.title} </h2>
+      <p className="mb-20">{data.p1} </p>
+      <h2 className="mb-7">{data.title2} </h2>
       <p>{data.p2} </p>
     </div>
   );
